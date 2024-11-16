9abe1bd79ea47389cf980843f35a8c4ab39a6289
Nguyen Quy <nquy50771@gmail.com>
2024-11-16 21:55:54 +0700

    upload source code 4

--- backend/script.js
+++ backend/script.js
@@ -1,14 +1,10 @@
 var swtBtn = document.querySelector('.nut2 input');
 var thebody = document.querySelector('body');
 var setting = document.querySelector('.seting-and-user i');
-var userSt = document.querySelector('.seting-and-user img');
 var userStHiden = document.querySelector('.seting-and-user');
 var clickSetting = document.querySelector('.setting');
-var clickSetting1 = document.querySelector('.setting1');
 var hidenSetting = document.querySelector('.setting.demo');
-var hidenSetting1 = document.querySelector('.setting1.demo1');
 var wrapperBtn = document.querySelector('.wrapper-btn');
-var wrapperBtn1 = document.querySelector('.setting1 .wrapper-btn');
 var searchInput = document.querySelector('.search-input input');
 var userChats = document.querySelectorAll('.user-chat');
 var groupUser = document.querySelector('.group-user');
@@ -20,21 +16,13 @@ swtBtn.addEventListener('change', () => {
 setting.addEventListener('click', () => {
     clickSetting.classList.add('demo')
 })
-userSt.addEventListener('click', () => {
-    clickSetting1.classList.add('demo1')
-})
 clickSetting.addEventListener('click', () => {
     clickSetting.classList.remove('demo');
-})
-clickSetting1.addEventListener('click', () => {
-    clickSetting1.classList.remove('demo1');
 })
 wrapperBtn.addEventListener('click', (e) => {
     e.stopPropagation();
 })
-wrapperBtn1.addEventListener('click', (e) => {
-    e.stopPropagation();
-})
+
 userChats.forEach(chat => {
     chat.addEventListener('click', function() {
         groupUser.classList.add('hidden');
@@ -48,4 +36,4 @@ backLeft.addEventListener('click', function() {
     userStHiden.classList.remove('hidden');
     chatBox.classList.remove('to-right');
     backLeft.classList.remove('display');
-})
+});
